901c5c072ebf5e8d0e86d651bea0007b6d936a83
Salah <2236291956@qq.com>
2022-11-04 17:18:51 +0800

    reactvitedemo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React, {
+  useEffect,
+  useRef,
+} from 'react';
+
+
+function App() {
+  return (
+    <>
+      <div>App</div>
+    </>
+  );
+}
+
+export default App;
